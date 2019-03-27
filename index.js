b359c1b3bbde48e30b80d8cfe3807f4237aa6cf2
schkiho <schkiho@yahoo.de>
2019-03-27 11:26:25 +0100

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
 class Calc {
   constructor(num) {
-    this.num = num
+    this.num = num;
   }
   add(numToAdd) {
-    this.throwOnBadDatatype(numToAdd)
-    this.num = this.num + numToAdd
-    return this
+    this.throwOnBadDatatype(numToAdd);
+    this.num = this.num + numToAdd;
+    return this;
+  }
+
+  substract(numToSubstract) {
+    this.throwOnBadDatatype(numToSubstract);
+    this.num = this.num - numToSubstract;
+    return this;
+  }
+
+  multiply(numToMultiply) {
+    this.throwOnBadDatatype(numToMultiply);
+    this.num = this.num * numToMultiply;
+    return this;
+  }
+
+  divide(numToDivide) {
+    this.throwOnBadDatatype(numToDivide);
+    this.num = this.num / numToDivide;
+    return this;
+  }
+
+  result() {
+    return this.num;
+  }
+
+  plus(num) {
+    return this.add(num);
+  }
+
+  minus(num) {
+    return this.substract(num);
+  }
+
+  mal(num) {
+    return this.multiply(num);
+  }
+
+  durch(num) {
+    return this.divide(num);
   }
 
   throwOnBadDatatype(numToAdd) {
     if (typeof numToAdd === "string") {
-      throw new Error("No strings allowed")
+      throw new Error("No strings allowed");
     }
 
     if (typeof numToAdd === "object") {
-      throw new Error("No object allowed")
+      throw new Error("No object allowed");
     }
   }
 }
 
-module.exports = Calc
+module.exports = Calc;
--- index.test.js
+++ index.test.js
@@ -1,21 +1,54 @@
-const Calc = require("./index")
+const Calc = require("./index");
 
 describe("Calculator tests", () => {
   test("should be able to add 1 + 1", () => {
-    expect(new Calc(1).add(1)).toEqual({ num: 2 })
-  })
+    expect(new Calc(1).add(1)).toEqual({ num: 2 });
+  });
 
   test("should be able to add multiple numbers", () => {
-    expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 })
-  })
+    expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 });
+  });
+
+  test("should be able to substract 1 - 1", () => {
+    expect(new Calc(1).substract(1)).toEqual({ num: 0 });
+  });
+
+  test("should be able to substract multiple numbers", () => {
+    expect(new Calc(5).substract(1).substract(1)).toEqual({ num: 3 });
+  });
+
+  test("should be able to multiply 1 * 1", () => {
+    expect(new Calc(1).multiply(1)).toEqual({ num: 1 });
+  });
+
+  test("should be able to multiply multiple numbers", () => {
+    expect(new Calc(1).multiply(1).multiply(1)).toEqual({ num: 1 });
+  });
+
+  test("should be able to divide 1 / 1", () => {
+    expect(new Calc(1).divide(1)).toEqual({ num: 1 });
+  });
+
+  test("should be able to devide multiple numbers", () => {
+    expect(new Calc(1).divide(1).divide(1)).toEqual({ num: 1 });
+  });
+
+  test("should be able to return a result", () => {
+    expect(
+      new Calc(1)
+        .divide(1)
+        .divide(1)
+        .result()
+    ).toEqual(1);
+  });
 
   test("Dont accept strings", () => {
-    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed")
-  })
+    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed");
+  });
 
   test("Dont accept object", () => {
     expect(() => new Calc(100).add({ num: "onehundret" })).toThrow(
       "No object allowed"
-    )
-  })
-})
+    );
+  });
+});
